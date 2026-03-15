96d8ce8ca56933ea936277e860cbb4a60caf5c46
szzzc1288 <138572753+szzzc1288@users.noreply.github.com>
2026-03-15 19:34:23 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -28,7 +28,7 @@ git clone --depth 1 https://github.com/esirplayground/luci-app-poweroff && rm -r
 git clone --depth 1 https://github.com/destan19/OpenAppFilter && mvdir OpenAppFilter
 # git clone --depth 1 https://github.com/sirpdboy/luci-app-advanced
 git clone --depth 1 -b lede https://github.com/pymumu/luci-app-smartdns && rm -rf luci-app-smartdns/.git
-git clone --depth 1 https://github.com/pymumu/openwrt-smartdns smartdns && rm -rf openwrt-smartdns/.git
+git clone --depth 1 https://github.com/pymumu/openwrt-smartdns && rm -rf openwrt-smartdns/.git smartdns
 # git clone --depth 1 https://github.com/cyzzc/luci-app-vssr
 # git clone --depth 1 https://github.com/jerrykuku/lua-maxminddb
 # git clone --depth 1 https://github.com/QiuSimons/openwrt-mos && mv -n openwrt-mos/*mosdns ./ && mv -n openwrt-mos/dat ./ ; rm -rf openwrt-mos
