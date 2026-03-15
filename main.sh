1d47d6265700ebaecf206ef2a14b7d4f0e566e0c
szzzc1288 <138572753+szzzc1288@users.noreply.github.com>
2026-03-15 18:10:24 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -41,7 +41,6 @@ git clone --depth 1 https://github.com/sirpdboy/luci-app-advancedplus
 git clone --depth 1 https://github.com/kiddin9/kwrt-packages && mv -n kwrt-packages/luci-app-bypass kwrt-packages/luci-app-fileassistant ./ ; rm -rf kwrt-packages
 #svn export https://github.com/immortalwrt/packages/trunk/net/smartdns
 
-svn export https://github.com/immortalwrt/packages/trunk/net/smartdns
 
 git_sparse_clone master "https://github.com/immortalwrt/packages" "immpack" net/sub-web \
 net/smartdns net/dnsproxy net/haproxy net/v2raya net/cdnspeedtest \
