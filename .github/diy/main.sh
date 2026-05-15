63c9bd8669527b2fd00ca9b77c397d96ed136bc5
szzzc1288 <138572753+szzzc1288@users.noreply.github.com>
2026-05-15 15:25:08 +0800

    Update main.sh

--- .github/diy/main.sh
+++ .github/diy/main.sh
@@ -18,8 +18,8 @@ mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
 git clone --depth 1 https://github.com/sirpdboy/luci-theme-kucat.git && rm -rf luci-theme-kucat/.git
-git clone --depth 1 -b lede https://github.com/pymumu/luci-app-smartdns && rm -rf luci-app-smartdns/.git
-git clone --depth 1 https://github.com/pymumu/openwrt-smartdns smartdns && rm -rf smartdns/.git
+#git clone --depth 1 -b lede https://github.com/pymumu/luci-app-smartdns && rm -rf luci-app-smartdns/.git
+#git clone --depth 1 https://github.com/pymumu/openwrt-smartdns smartdns && rm -rf smartdns/.git
 git clone --depth 1 https://github.com/cyzzc/openwrt_nezha && mvdir openwrt_nezha
 git clone --depth 1 https://github.com/sirpdboy/luci-app-advancedplus && rm -rf luci-app-advancedplus/.git
 
